6165c8745144790efb26de2a29ddee24481b54e2
Rakesh <rakesh.mitra@outlook.com>
2018-12-04 22:06:58 +0530

    Day2

--- assets/testStreams.js
+++ assets/testStreams.js
@@ -21,8 +21,8 @@ myreadStream.pipe(mywritePipeStream);
 //Sending the contents to client from server
 var server=http.createServer(function(req,res){
   console.log('Received request from client');
-  res.writeHead(200,{'Content-Type':'text/plain'});
-  var mynewReadStream = fs.createReadStream(__dirname + '/readStory.txt', 'utf8');
+  res.writeHead(200,{'Content-Type':'text/html'});
+  var mynewReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
   mynewReadStream.pipe(res);
 
 });
