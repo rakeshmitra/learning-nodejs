e88c5a1e4053958d697d21c67d24c6d9af520d69
Rakesh <rakesh.mitra@outlook.com>
2018-12-06 08:50:28 +0530

    Day3

--- assets/expressWithTemplateEngine.js
+++ assets/expressWithTemplateEngine.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var exp = express();
 
+//Using Tmaplate engine
 exp.set('view engine','ejs');
 
+//Serving Static contents
+exp.use('/images', express.static('images'));
+
+//To read Query String  use req.query
 
 exp.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
@@ -16,12 +22,24 @@ exp.get('/Home',function(req,res){
 
 exp.get('/Contact',function(req,res){
   //res.sendFile(__dirname + '/contact.html');
-  res.render('contact');
+  res.render('contact', {qStr : req.query});
 });
 
 exp.get('/profile/:name',function(req,res){
   var userData = {age: '35yrs' , job:'Engineer', hobbies :['eating','playing','sleeping']};
   res.render('profile',{userName : req.params.name, userData:userData});
 });
+
+//Body-parser package code to read values from POST method in form
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// POST /login gets urlencoded bodies
+exp.post('/ContactSuccess', urlencodedParser, function (req, res) {
+  console.log(req.body);
+  if (!req.body) return res.sendStatus(400)
+  //res.send('welcome, ' + req.body.who);
+  res.render('contact-success', {data : req.body});
+})
 
 exp.listen(8080);
